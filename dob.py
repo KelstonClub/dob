122be30e267018fbcb6b52536f1f653bd6f20494
Tim Golden <mail@timgolden.me.uk>
2020-03-14 17:05:09 +0000

    Housekeeping

--- dob.py
+++ dob.py
@@ -1,27 +1,43 @@
 # Write your code here :-)
 from datetime import date
 
-dob = input("DoB [yyyymmdd]: ")
-print(dob)
-if int(dob) < 10000000:
-    raise RuntimeError("Invalid date of birth")
-yyyy = dob[:4]
-# yyyy = ""
-# for i in range(4):
-#     yyyy += dob[i]
-print(yyyy)
-mm = dob[4:6]
-print(mm)
-dd = dob[6:]
-print(dd)
+def get_dob():
+    dob = input("DoB [yyyymmdd]: ")
+    if int(dob) < 10000000:
+        raise RuntimeError("Invalid date of birth")
+    yyyy = dob[:4]
+    mm = dob[4:6]
+    dd = dob[6:]
+    return int(yyyy), int(mm), int(dd)
 
-year = int(yyyy)
-month = int(mm)
-day = int(dd)
-print(year, month, day)
+def get_today():
+    today = date.today()
+    tyear = today.year
+    tmonth = today.month
+    tday = today.day
+    return tyear, tmonth, tday
 
-today = date.today()
-tyear = today.year
-tmonth = today.month
-tday = today.day
-print(tyear, tmonth, tday)
+def calculate_age(dob, today):
+    #
+    # Unpack the dob & today into
+    # individual elements
+    #
+    year, month, day = dob
+    tyear, tmonth, tday = today
+
+    age = tyear - year
+    if tmonth >= month:
+        if tmonth == month:
+            if tday < day:
+                age -= 1
+            elif tday == day:
+                print("Congratulations!")
+    else:
+        age -= 1
+
+    return age
+
+dob = get_dob()
+today = get_today()
+age = calculate_age(dob, today)
+print("You are", age, "years old")
---
+++ dob2.py
@@ -0,0 +1,10 @@
+tday = 14
+tmonth = 2
+day = 14
+
+if tday == day:
+    print("Happy Birthday")
+if tmonth == 2 and tday == 14:
+    print("Happy Valentine")
+if tmonth == 12 and tday == 25:
+    print("Happy Christmas")
